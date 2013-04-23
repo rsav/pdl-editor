e819a538d739e9aec9f468df0851a54728d0c274
renaud.savalle@gmail.com <renaud.savalle@gmail.com@9f4a9500-e6a9-49fc-d86d-e9185e310f86>
2013-04-23 14:50:59 +0000

    version 0.5: see changes in doc/CHANGES.txt

--- trunk/PDLEditor/src/net/ivoa/pdl/editor/dialog/FunctionExpressionDialog.java
+++ trunk/PDLEditor/src/net/ivoa/pdl/editor/dialog/FunctionExpressionDialog.java
@@ -5,58 +5,66 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.TreeMap;
 
-import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JDialog;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
+import javax.swing.ComboBoxModel;
 
 import net.ivoa.parameter.model.AtomicConstantExpression;
+import net.ivoa.parameter.model.AtomicParameterExpression;
 import net.ivoa.parameter.model.Expression;
+import net.ivoa.parameter.model.Function;
+import net.ivoa.parameter.model.FunctionExpression;
+import net.ivoa.parameter.model.FunctionType;
 import net.ivoa.parameter.model.Operation;
 import net.ivoa.parameter.model.OperationType;
+import net.ivoa.parameter.model.ParameterReference;
 import net.ivoa.parameter.model.ParameterType;
+import net.ivoa.parameter.model.ParenthesisContent;
 import net.ivoa.pdl.editor.guiComponent.MapComboBoxModel;
+import net.ivoa.pdl.editor.objectModel.PDLParameter;
 
-public class AtomicConstantExpressionDialog extends JDialog {
+public class FunctionExpressionDialog extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
-	
 	private JTextField textFieldName;
-	private JTextField textFieldConstant;
-
-	// application objects for the available expressions
-	private TreeMap<String, Expression> mapExps;
+	private TreeMap<String,Expression> mapExps;
+	
+	private TreeMap<String, Expression> mapExpsForOperand;
+	private MapComboBoxModel comboBoxModelOperand;
+	
+	private TreeMap<String, Expression> mapExpsForPower;
+	private MapComboBoxModel comboBoxModelPower;
+	
+	
 	private MapComboBoxModel comboBoxModelExps;
 	private JComboBox comboBoxExps;
-
-	private MapComboBoxModel comboBoxModelPower;
-
-
-	private MapComboBoxModel comboBoxModelOperand;
-
-	private TreeMap<String, Expression> mapExpsForOperand;
-
-	private TreeMap<String, Expression> mapExpsForPower;
-
-
-
+	
+	private TreeMap<String, PDLParameter> mapParams;
+	private MapComboBoxModel comboBoxModelExpression;
+	
+	
+	
 
 	/**
 	 * Create the dialog.
 	 */
-	public AtomicConstantExpressionDialog(TreeMap<String,Expression> mte, MapComboBoxModel mce, JComboBox ce) {
+	public FunctionExpressionDialog(TreeMap<String,Expression> me, MapComboBoxModel mce, JComboBox ce, TreeMap<String,PDLParameter> mp) {
 		
-		mapExps = mte;
+		mapExps = me;
 		comboBoxModelExps = mce;
 		comboBoxExps = ce;
+		mapParams = mp;
+		
 		
 		
 		setBounds(100, 100, 450, 300);
@@ -68,67 +76,63 @@ public class AtomicConstantExpressionDialog extends JDialog {
 		JLabel lblName = new JLabel("Name:");
 		lblName.setBounds(6, 6, 61, 16);
 		contentPanel.add(lblName);
-	
 		
 		textFieldName = new JTextField();
 		textFieldName.setBounds(79, 0, 134, 28);
 		contentPanel.add(textFieldName);
 		textFieldName.setColumns(10);
 		
-		JLabel lblType = new JLabel("Type:");
-		lblType.setBounds(6, 34, 61, 16);
-		contentPanel.add(lblType);
+		JLabel lblExpression = new JLabel("Expression:");
+		lblExpression.setBounds(6, 34, 75, 16);
+		contentPanel.add(lblExpression);
 		
-		final JComboBox comboBoxType = new JComboBox(ParameterType.values());
-		comboBoxType.setBounds(79, 30, 134, 27);
-		contentPanel.add(comboBoxType);
 		
-		JLabel lblConstant = new JLabel("Constant:");
-		lblConstant.setBounds(6, 62, 61, 16);
-		contentPanel.add(lblConstant);
-		
-		textFieldConstant = new JTextField();
-		textFieldConstant.setBounds(79, 56, 134, 28);
-		contentPanel.add(textFieldConstant);
-		textFieldConstant.setColumns(10);
+		comboBoxModelExpression = new MapComboBoxModel(mapExps);
+		final JComboBox comboBoxExpression = new JComboBox(comboBoxModelExpression);
+		comboBoxExpression.setBounds(79, 30, 134, 27);
+		contentPanel.add(comboBoxExpression);
 		
 		JLabel lblPower = new JLabel("Power:");
-		lblPower.setBounds(225, 6, 61, 16);
+		lblPower.setBounds(225, 4, 61, 16);
 		contentPanel.add(lblPower);
 		
 		JLabel lblOperation = new JLabel("Operation:");
-		lblOperation.setBounds(225, 33, 81, 16);
+		lblOperation.setBounds(225, 31, 81, 16);
 		contentPanel.add(lblOperation);
 		
-		
-	    final JComboBox comboBoxOperation = new JComboBox(OperationType.values());
-		comboBoxOperation.setBounds(302, 29, 121, 27);
-		
+		final JComboBox comboBoxOperation = new JComboBox(OperationType.values());
+		comboBoxOperation.setBounds(302, 27, 121, 27);
 		contentPanel.add(comboBoxOperation);
 		
-		JLabel lblOperand = new JLabel("Operand");
-		lblOperand.setBounds(225, 61, 61, 16);
+		JLabel lblOperand = new JLabel("Operand:");
+		lblOperand.setBounds(225, 59, 61, 16);
 		contentPanel.add(lblOperand);
+		
 		
 		mapExpsForOperand = new TreeMap<String, Expression>(mapExps);
 		mapExpsForOperand.put("",null); // since the operation is not mandatory, we provide a choice for no operand
-		
 		comboBoxModelOperand = new MapComboBoxModel(mapExpsForOperand);
+		
 		final JComboBox comboBoxOperand = new JComboBox(comboBoxModelOperand);
-		comboBoxOperand.setBounds(302, 57, 121, 27);
+		comboBoxOperand.setBounds(302, 55, 121, 27);
 		contentPanel.add(comboBoxOperand);
 		
 		mapExpsForPower = new TreeMap<String, Expression>(mapExps);
 		mapExpsForPower.put("",null); // since the power is not mandatory, we provide a choice for no expression
 		
 		comboBoxModelPower = new MapComboBoxModel(mapExpsForPower);
+		
 		final JComboBox comboBoxPower = new JComboBox(comboBoxModelPower);
-		comboBoxPower.setBounds(302, 2, 121, 27);
+		comboBoxPower.setBounds(302, 0, 121, 27);
 		contentPanel.add(comboBoxPower);
 		
+		JLabel lblFunction = new JLabel("Function:");
+		lblFunction.setBounds(6, 62, 61, 16);
+		contentPanel.add(lblFunction);
 		
-		
-		
+		final JComboBox comboBoxFunction = new JComboBox(FunctionType.values());
+		comboBoxFunction.setBounds(79, 58, 134, 27);
+		contentPanel.add(comboBoxFunction);
 		
 		JPanel buttonPane = new JPanel();
 		buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
@@ -138,7 +142,7 @@ public class AtomicConstantExpressionDialog extends JDialog {
 		okButton.setActionCommand("OK");
 		buttonPane.add(okButton);
 		getRootPane().setDefaultButton(okButton);
-		
+	
 		okButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 
@@ -146,7 +150,7 @@ public class AtomicConstantExpressionDialog extends JDialog {
 				String newName = textFieldName.getText();
 				
 				if(newName.isEmpty()) {
-					JOptionPane.showMessageDialog(getContentPane(),"No name entered. Cannot create AtomicConstantExpression","Error",JOptionPane.ERROR_MESSAGE);
+					JOptionPane.showMessageDialog(getContentPane(),"No name entered. Cannot create FunctionExpression","Error",JOptionPane.ERROR_MESSAGE);
 				} else {
 				
 					
@@ -161,27 +165,15 @@ public class AtomicConstantExpressionDialog extends JDialog {
 					if(nameUsed==false) {
 						
 						
+						// get the expression selected
+						String expression = (String) comboBoxExpression.getSelectedItem();
+						Expression newExpression = mapExps.get(expression);
+						
+						// get the function selected
+						FunctionType newFunctionType = (FunctionType) comboBoxFunction.getSelectedItem();
 						
 						
-						
-						// get the type selected in the combo box
-						ParameterType newType = (ParameterType) comboBoxType.getSelectedItem();
-						
-						
-						
-						
-						
-						// get the constant text
-						String newConstant = textFieldConstant.getText();
-						
-						// parse the string representing a vector
-						String delims = ";";
-						String[] newConstants = newConstant.split(delims);
-						
-						// put the constants in a list
-						List<String> newConstantList = Arrays.asList(newConstants); 
-						
-						// get the power
+						// get the power selected
 						String power = (String) comboBoxPower.getSelectedItem();
 						Expression newPowerExpression = mapExps.get(power);
 						
@@ -200,17 +192,22 @@ public class AtomicConstantExpressionDialog extends JDialog {
 							newOperation = new Operation(newOperandExpression,newOperationType);
 						}
 						
-						System.out.println("DEBUG AtomicConstantExpressionDialog.okButton: Creating new AtomicConstantExpression:");
-						System.out.println("DEBUG AtomicConstantExpressionDialog.okButton: type="+newType);
-						System.out.println("DEBUG AtomicConstantExpressionDialog.okButton: constants="+newConstantList);
-						System.out.println("DEBUG AtomicConstantExpressionDialog.okButton: power="+newPowerExpression);
-						System.out.println("DEBUG AtomicConstantExpressionDialog.okButton: operation="+newOperation);
+						
+						// create the new function
+						Function newFunction = new Function()
+														.withExpression(newExpression)
+														.withFunctionName(newFunctionType);
+						
+						System.out.println("DEBUG FunctionExpressionDialog.okButton: Creating new FunctionExpression:");
+						System.out.println("DEBUG FunctionExpressionDialog.okButton: expression="+newExpression);
+						System.out.println("DEBUG FunctionExpressionDialog.okButton: function="+newFunction);
+						System.out.println("DEBUG FunctionExpressionDialog.okButton: power="+newPowerExpression);
+						System.out.println("DEBUG FunctionExpressionDialog.okButton: operation="+newOperation);
 							
 						
-						// create the new expression of type AtomicConstantExpression
-						AtomicConstantExpression newExp = new AtomicConstantExpression()
-															.withConstantType(newType)
-															.withConstant(newConstantList)
+						// create the new expression of type FunctionExpression
+						FunctionExpression newExp = new FunctionExpression()
+															.withFunction(newFunction)
 															.withPower(newPowerExpression)
 															.withOperation(newOperation);
 															
@@ -219,7 +216,7 @@ public class AtomicConstantExpressionDialog extends JDialog {
 						
 						
 						// add the expression to the model
-						System.out.println("DEBUG AtomicConstantExpressionDialog.okButton: Adding new expression to mapExps");
+						System.out.println("DEBUG FunctionExpressionDialog.okButton: Adding new expression to mapExps");
 						mapExps.put(newName,newExp); 
 						
 						// signals the model that we have updated the hash
@@ -236,20 +233,18 @@ public class AtomicConstantExpressionDialog extends JDialog {
 				}
 			}
 		});
-			
-			
 		
 		
 		JButton cancelButton = new JButton("Cancel");
 		cancelButton.setActionCommand("Cancel");
 		buttonPane.add(cancelButton);
-		
+
 		cancelButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dispose(); // close the window
 			}
 		});
 		
-		
+	
 	}
 }
